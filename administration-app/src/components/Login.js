f83e7c549ac3fa0fff1b8990aa059dd9cb665731
nmahic1 <nmahic1@etf.unsa.ba>
2023-03-24 13:30:29 +0100

    changes

--- administration-app/src/components/Login.js
+++ administration-app/src/components/Login.js
@@ -3,26 +3,24 @@ import "./Login.css"
 import Axios from 'axios'
 import { useNavigate } from "react-router-dom";
 
-
 const LoginForm = () => {
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     return ( 
         <div className="App1"> 
             <div className="cover">
-                    <h1>Prijavi se</h1>
-                    <input type="text" placeholder="username" onChange={(e)=>{
-                       // setUsername(e.target.value)
-                    }}/>
-                    <input type="password" placeholder="password" onChange={(e)=>{
-                        //setPassword(e.target.value)
-                    }}/>
-                    <p>Nisi registrovan.<a href="/">  Registruj se</a></p>
-                    <button className="login-btn"> Logiraj se</button>
-                                
+                <h1>Prijavi se</h1>
+                <input type="text" placeholder="username" onChange={(e)=>{
+                    setUsername(e.target.value)
+                }}/>
+                <input type="password" placeholder="password" onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}/>
+                <p>Nisi registrovan.<a href="/">  Registruj se</a></p>
+                <button className="login-btn"> Logiraj se</button>                          
             </div>
-       </div>
-       
+        </div>
     );
 }
  
