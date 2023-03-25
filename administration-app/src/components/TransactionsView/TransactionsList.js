503a79e04740f75bbd5fd84b585c4313c3ed8f6a
Mirza Kobasevic <mkobasevic1@etf.unsa.ba>
2023-03-25 14:21:25 +0100

    minor style changes, heading added

--- administration-app/src/components/TransactionsView/TransactionsList.js
+++ administration-app/src/components/TransactionsView/TransactionsList.js
@@ -1,54 +1,66 @@
-import { getBasicTransactions } from '../../services/TransactionsView/transactionsService';
-import TransactionsListHeader from './TransactionsListHeader';
-import Transaction from './Transaction';
-import { useState, useEffect } from 'react';
-import './Transaction.css';
-import TransactionDetails from './TransactionDetails';
-import React from 'react';
+import { getBasicTransactions } from "../../services/TransactionsView/transactionsService";
+import TransactionsListHeader from "./TransactionsListHeader";
+import Transaction from "./Transaction";
+import { useState, useEffect } from "react";
+import "./Transaction.css";
+import TransactionDetails from "./TransactionDetails";
+import React from "react";
 
 export const TransactionsList = () => {
-	const [details, setDetails] = useState(null);
-	const [transactions, setTransactions] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [hasMore, setHasMore] = useState(true);
-	var counter = 1;
+  const [details, setDetails] = useState(null);
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  var counter = 1;
 
-	useEffect(() => {
-		loadTransactions();
-		window.addEventListener('scroll', handleScroll);
-	}, []);
+  useEffect(() => {
+    loadTransactions();
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
-	function loadTransactions() {
-		setIsLoading(true);
-		console.log(hasMore);
-		console.log(counter);
-		//fetch() or axios.get()
-		var result = getBasicTransactions(counter * 15);
-		var transactionsdata = result.data.map((item, index) => (
-			<Transaction setDetails={setDetails} index={index} prop={item}></Transaction>
-		));
-		setTransactions(transactionsdata);
-		setHasMore(result.hasMore);
-		counter = counter + 1;
-		setIsLoading(false);
-	}
+  function loadTransactions() {
+    setIsLoading(true);
+    console.log(hasMore);
+    console.log(counter);
+    //fetch() or axios.get()
+    var result = getBasicTransactions(counter * 15);
+    var transactionsdata = result.data.map((item, index) => (
+      <Transaction
+        setDetails={setDetails}
+        index={index}
+        prop={item}
+      ></Transaction>
+    ));
+    setTransactions(transactionsdata);
+    setHasMore(result.hasMore);
+    counter = counter + 1;
+    setIsLoading(false);
+  }
 
-	function handleScroll(e) {
-		if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 0.1) {
-			loadTransactions();
-		}
-	}
+  function handleScroll(e) {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      0.1
+    ) {
+      loadTransactions();
+    }
+  }
 
-	return (
-		<div className='transactions-root'>
-			{details != null ? (
-				<TransactionDetails setDetails={setDetails} props={details}></TransactionDetails>
-			) : (
-				<div>
-					<TransactionsListHeader></TransactionsListHeader> {transactions}
-				</div>
-			)}
-			{isLoading && <div>Loading...</div>}
-		</div>
-	);
+  return (
+    <div className="transactions-root">
+      {details != null ? (
+        <TransactionDetails
+          setDetails={setDetails}
+          props={details}
+        ></TransactionDetails>
+      ) : (
+        <div>
+          <h2 className="heading">Transactions</h2>
+          <TransactionsListHeader></TransactionsListHeader> {transactions}
+        </div>
+      )}
+      {isLoading && <div>Loading...</div>}
+    </div>
+  );
 };
