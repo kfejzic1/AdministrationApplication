3f005ffe498dd245d3845ce336bceb44cc48e85e
Amina Kurtovic <akurtovic4@etf.unsa.ba>
2023-03-26 11:56:00 +0200

    Added migrate function

--- AdministrationAPI/Controllers/VendorController.cs
+++ AdministrationAPI/Controllers/VendorController.cs
@@ -1,6 +1,8 @@
 ﻿using AdministrationAPI.Contracts.Requests;
+using AdministrationAPI.Models;
 using AdministrationAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 namespace AdministrationAPI.Controllers
@@ -16,6 +18,9 @@ namespace AdministrationAPI.Controllers
         {
             _vendorService = vendorService;
             _context = context;
+
+            // Call EnsureCreated() method to create tables in the database if they don't exist
+            _context.Database.EnsureCreated();
         }
 
         [HttpPost]
@@ -37,6 +42,21 @@ namespace AdministrationAPI.Controllers
             }
         }
 
+        [HttpPost("migrate")]
+        public IActionResult Migrate()
+        {
+            try
+            {
+                _context.Database.Migrate();
+                return Ok("Migration successful.");
+            }
+            catch (Exception ex)
+            {
+                LoggerUtility.Logger.LogException(ex, "VendorController.Migrate");
+                return StatusCode(500, ex.Message);
+            }
+        }
+
         [HttpGet]
         public IActionResult GetVendors()
         {
@@ -45,5 +65,3 @@ namespace AdministrationAPI.Controllers
         }
     }
 }
-
-
