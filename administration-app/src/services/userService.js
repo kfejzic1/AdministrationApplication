a617b34c8c74aa784625bcfeb8a42ae3d9be180e
amarhasanovic <ahasanovic4@etf.unsa.ba>
2023-03-26 13:40:22 +0200

    get users service added

--- administration-app/src/services/userService.js
+++ administration-app/src/services/userService.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { env } from '../config/env';
 
-export function createVendor(request) {
-	return axios(env.API_ENV.url + '/api/Vendor', {
-		method: 'POST',
-		data: request,
+export function getAllUsers() {
+	return axios(env.API_ENV.url + '/api/User', {
+		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
 			Authorization: localStorage.getItem('token'),
