c504772c9131145e9ea56073df5f710a4a9d161a
amarhasanovic <ahasanovic4@etf.unsa.ba>
2023-03-26 12:44:24 +0200

    back connection

--- AdministrationAPI/Data/AppDbContext.cs
+++ AdministrationAPI/Data/AppDbContext.cs
@@ -6,6 +6,8 @@ namespace AdministrationAPI.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -18,7 +20,6 @@ namespace AdministrationAPI.Data
             var connectionString = configuration.GetConnectionString("DefaultConnectionString");
 
             Console.WriteLine("Default connection string: " + connectionString);
-
 
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
