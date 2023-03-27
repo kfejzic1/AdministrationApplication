d5bc75a37bc0881317685797a6d4474af7c66d9e
Elvedin Smajić <esmajic2@etf.unsa.ba>
2023-03-28 00:05:38 +0200

    Added redirect after login and error message

--- administration-app/src/components/Login.js
+++ administration-app/src/components/Login.js
@@ -2,37 +2,44 @@ import React, { useState } from "react";
 import "./Login.css"
 import { useNavigate } from "react-router-dom";
 import { loginFunction } from "../services/loginServices";
+import { Button, Typography, TextField, Input, Alert } from "@mui/material"
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [response, setResponse] = useState('');
+    const [errorMessage, setErrorMessage] = useState(''); 
 
     const handleButtonClick = () => {
 		loginFunction(email,password).then(res => {
-			setResponse(res.data);
-		});
+            setErrorMessage("");
+            window.location.href = 'http://localhost:3000/twofactor';
+		}).catch(err => {
+            setErrorMessage("Neuspješna prijava!");
+        });
 	};
-
-    console.log(response);
 
     return ( 
         <div className="App1"> 
-            <div className="cover">                      
-                    <h1>Login</h1>
-                    <input className="user-data" type="text" placeholder="E-mail or Phone number" onChange={(e)=>{
-                       setEmail(e.target.value)
-                    }}/>
-
-                    <input className="user-data" type="password" placeholder="Password" onChange={(e)=>{
-                      setPassword(e.target.value)
-                    }}/>
-                    <p>You are not registered? <a href="/">Register</a></p>
-                    <button className="login-btn" onClick={handleButtonClick}> Login</button>
-                                
+            <div className="cover">                  
+                <Typography variant="h4">Login</Typography>
+                <Alert severity="error" variant="filled" style={{display: "none"}}>{errorMessage}</Alert>
+                <div classname="alert-box">
+                    <Typography variant="h5">{errorMessage}</Typography>
+                </div>  
+                <Input className="user-data" type="text" placeholder="E-mail or Phone number" onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}/>
+                <Input className="user-data" type="password" placeholder="Password" onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}/>
+                <Typography>You are not registered? <a href="/">Register</a></Typography>
+                <button className="login-btn" onClick={handleButtonClick}> Login</button>
             </div>
         </div>
     );
+
 }
- 
+
 export default LoginForm;
+            
--- administration-app/src/services/loginServices.js
+++ administration-app/src/services/loginServices.js
@@ -2,20 +2,21 @@ import axios from 'axios';
 import { env } from '../config/env';
 
 export function loginFunction(email, password) {
-    const url = `${env.API_ENV.url}/login`;
+    const url = `${env.API_ENV.url}/api/User/login`;
     const data = { email, password };
 
     return axios.post(url, data, {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Credentials': true,
+          'Access-Control-Allow-Origin': '*'
         },
-        withCredentials: true
       });
 }
 
 
 export function twoFactorAut(token){
-  const url = `${env.API_ENV.url}/login2FA`;
+  const url = `${env.API_ENV.url}/api/User/login2FA`;
   const data = {token}
   return axios.post(url, data, {
     headers:{
