0544ead00f1cce7ace6152e7db72c96fc4edb1d1
Din Švraka <dsvraka1@etf.unsa.ba>
2023-03-29 18:24:31 +0200

    B2BcustomersPanel - 1. verzija

---
+++ administration-app/src/components/table/B2BTable/VendorsListHeader.js
@@ -0,0 +1,17 @@
+import './css/Vendors.css';
+export default function VendorsListHeader() {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Address</th>
+					<th>Details</th>
+					<th>Phone number</th>
+					<th>Assigned Users</th>
+				</tr>
+			</thead>
+		</table>
+	);
+}
