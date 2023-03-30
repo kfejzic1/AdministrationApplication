28426aba79981e72c4c7b7f615a0d6b373015f1c
tdogic2 <tdogic2@etf.unsa.ba>
2023-03-30 21:43:28 +0200

    Changed name of context

--- AdministrationAPI/Program.cs
+++ AdministrationAPI/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
