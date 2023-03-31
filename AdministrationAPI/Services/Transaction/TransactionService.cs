c041b408d2173258dcda60fb0df922ac4b5386dc
tdogic2 <tdogic2@etf.unsa.ba>
2023-03-31 11:07:54 +0200

    Combined pagination, filter and sort into one endpoint so that it can be called in any way

--- AdministrationAPI/Services/Transaction/TransactionService.cs
+++ AdministrationAPI/Services/Transaction/TransactionService.cs
@@ -19,44 +19,72 @@ namespace AdministrationAPI.Services.Transaction
 
         }
 
-        public async Task<TransactionResponseDTO> GetAllTransactions(int pageNumber, int pageSize)
+        public async Task<TransactionResponseDTO> GetTransactions(TransactionQueryOptions options)
         {
 
-            if (pageNumber < 0) throw new ArgumentException("Page number cannot be negative");
-            if (pageSize < 0) throw new ArgumentException("Page size cannot be negative");
-
-            if (pageNumber == 0) pageNumber = 1;
-            if (pageSize == 0) pageSize = _context.Transactions.Count();
-
-            var pageCount = Math.Ceiling(_context.Transactions.Count() / (double)pageSize);
-
-            var dbTransactions = await _context.Transactions
-                .Skip((pageNumber - 1) * (int)pageSize)
-                .Take((int)pageSize).ToListAsync();
-                
-            var response = new TransactionResponseDTO
+            // Throw error if pageNumber or pageSize is less than 1
+            if (options.PageNumber < 1 || options.PageSize < 1)
             {
-                Transactions = dbTransactions.Select(transaction => _mapper.Map<TransactionDTO>(transaction)).ToList(),
-                CurrentPage = pageNumber,
-                Pages = (int)pageCount
+                throw new Exception("PageNumber and PageSize must be greater than or equal to 1.");
+            }
 
-            };
-            if (response.Transactions.Count == 0) throw new Exception("There are no transactions.");
+            var transactions = _context.Transactions.AsQueryable();
+
+            // Filter transactions
+            if (options.DateTimeStart == null && options.DateTimeEnd != null) options.DateTimeStart = options.DateTimeEnd;
+            if (options.DateTimeEnd == null && options.DateTimeStart != null) options.DateTimeEnd = options.DateTimeStart;
+
+            if (options.DateTimeStart != null && options.DateTimeEnd != null)
+            {
+                transactions = transactions.Where(t => t.DateTime >= options.DateTimeStart && t.DateTime <= options.DateTimeEnd);
+            }
+
+            if (!string.IsNullOrEmpty(options.Recipient))
+            {
+                transactions = transactions.Where(t => t.Recipient.ToLower() == options.Recipient.ToLower());
+            }
+
+            if (options.MinAmount == null && options.MaxAmount != null) options.MinAmount = options.MaxAmount;
+            if (options.MaxAmount == null && options.MinAmount != null) options.MaxAmount= options.MinAmount;
+
+            if (options.MinAmount != null && options.MaxAmount != null)
+            {
+                transactions = transactions.Where(t => t.Amount >= options.MinAmount && t.Amount <= options.MaxAmount);
+            }
+
+            if (options.Status != null)
+            {
+                transactions = transactions.Where(t => t.Status == options.Status);
+            }
+
+            // Sort transactions
+            if (options.SortingOptions != null)
+            {
+                if (options.SortingOptions == SortingOptions.Amount) 
+                    transactions = (options.Ascending == true) ? transactions.OrderBy(t => t.Amount) : transactions.OrderByDescending(t => t.Amount);
+                else if (options.SortingOptions == SortingOptions.Recipient) 
+                    transactions = (options.Ascending == true) ? transactions.OrderBy(t => t.Recipient) : transactions.OrderByDescending(t => t.Recipient);
+                else 
+                    transactions = (options.Ascending == true) ? transactions.OrderBy(t => t.DateTime) : transactions.OrderByDescending(t => t.DateTime);
+            }
+
+            var response = new TransactionResponseDTO();
+
+            // Set default values for pageNumber and pageSize
+            int pageNumber = options.PageNumber ?? 1;
+            int pageSize = options.PageSize ?? await transactions.CountAsync();
+
+            // Calculate skip count based on pageNumber and pageSize
+            int skipCount = (pageNumber - 1) * pageSize;
+
+            // Apply pagination
+            transactions = transactions.Skip(skipCount).Take(pageSize);
+
+            response.Transactions = await transactions.Select(transaction => _mapper.Map<TransactionDTO>(transaction)).ToListAsync();
+            response.Pages = (int)Math.Ceiling((double)await transactions.CountAsync() / pageSize);
+            response.CurrentPage = pageNumber;
 
             return response;
-        }
-        public async Task<List<TransactionDTO>> GetSortedTransactions(SortingOptions sortingOptions, bool ascending = true)
-        {
-            var transactions = _context.Transactions.AsQueryable();
-            var orderedTransactions = transactions;
-
-            if (sortingOptions == SortingOptions.Amount) orderedTransactions = ascending ? transactions.OrderBy(t => t.Amount) : transactions.OrderByDescending(t => t.Amount);
-            else if (sortingOptions == SortingOptions.Recipient) orderedTransactions = ascending ? transactions.OrderBy(t => t.Recipient) : transactions.OrderByDescending(t => t.Recipient);
-            else orderedTransactions = ascending ? transactions.OrderBy(t => t.DateTime) : transactions.OrderByDescending(t => t.DateTime);
-            
-            var dbTransactions = await orderedTransactions.ToListAsync();
-
-            return dbTransactions.Select(transaction => _mapper.Map<TransactionDTO>(transaction)).ToList();
         }
 
         public async Task<TransactionDetailsDTO> GetTransactionByID(int id)
@@ -67,41 +95,5 @@ namespace AdministrationAPI.Services.Transaction
 
             return _mapper.Map<TransactionDetailsDTO>(dbTransaction);
         }
-
-        public async Task<List<TransactionDTO>> GetTransactionsByFilter(DateTime? dateTimeStart = null, DateTime? dateTimeEnd = null, string? recipient = null, int? amountMin = null, int? amountMax = null, TransactionStatus? status = null)
-        {
-            var transactions = _context.Transactions.AsQueryable();
-
-            if (dateTimeStart == null && dateTimeEnd != null) dateTimeStart = dateTimeEnd;
-            if (dateTimeEnd == null && dateTimeStart != null) dateTimeEnd = dateTimeStart;
-
-            if (amountMin == null && amountMax != null) amountMin = amountMax;
-            if (amountMax == null && amountMin != null) amountMax = amountMin;
-
-            if (dateTimeStart != null && dateTimeEnd != null)
-            {
-                transactions = transactions.Where(t => t.DateTime >= dateTimeStart && t.DateTime <= dateTimeEnd);
-            }
-
-            if (recipient != null)
-            {
-                transactions = transactions.Where(t => t.Recipient == recipient);
-            }
-
-            if (amountMin != null && amountMax != null && amountMin != 0 && amountMax != 0)
-            {
-                transactions = transactions.Where(t => t.Amount >= amountMin && t.Amount <= amountMax);
-            }
-
-            if (status != null && amountMin != 0 && amountMax != 0)
-            {
-                transactions = transactions.Where(t => t.Status == status);
-            }
-
-            var dbTransactions = await transactions.ToListAsync();
-
-            return dbTransactions.Select(transaction => _mapper.Map<TransactionDTO>(transaction)).ToList();
-        }
-
     }
 }
