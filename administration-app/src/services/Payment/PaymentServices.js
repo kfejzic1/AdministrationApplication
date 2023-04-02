9e15ddf2b27739acedb92b81d4f0a0937e81bef4
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-04-02 21:55:32 +0200

    payment api call fixed

--- administration-app/src/services/Payment/PaymentServices.js
+++ administration-app/src/services/Payment/PaymentServices.js
@@ -5,15 +5,8 @@ export function sendPaymentInfo(info) {
 	return axios(env.ANDROID_API_ENV.url + '/Transaction/CreateTransaction?token=' + 'TEST', {
 		method: 'POST',
 		data: info,
-		crossorigin: true,
-		mode: 'cors',
-		withCredentials: true,
 		headers: {
 			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Credentials': true,
-			'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-			'Access-Control-Allow-Headers': '*',
 		},
 	});
 }
