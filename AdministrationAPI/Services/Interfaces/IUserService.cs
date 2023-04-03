9e170406c55d0b389fb5890965f36e0a0e210412
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2023-04-03 12:07:57 +0200

    new services

--- AdministrationAPI/Services/Interfaces/IUserService.cs
+++ AdministrationAPI/Services/Interfaces/IUserService.cs
@@ -9,6 +9,9 @@ namespace AdministrationAPI.Services.Interfaces
         Task<AuthenticationResult> Login(LoginRequest loginRequest);
         Task<AuthenticationResult> Login2FA(Login2FARequest loginRequest);
         Task<UserDT> GetUser(string id);
+        User GetUserByEmail(string email);
+        User GetUserByFirstName(string firstName);
+        Task<User> Register(RegisterRequest model);
         Task<QRCodeResponse> GetTwoFactorQRCode(string id);
         Task<bool> Toggle2FA(string id);
         List<User> GetAllUsers();
