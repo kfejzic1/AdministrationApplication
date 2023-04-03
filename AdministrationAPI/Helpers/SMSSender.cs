da9cf6c5cec4b0eb594a8472ab85e9133c530eaf
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2023-04-03 12:06:52 +0200

    helper classes

---
+++ AdministrationAPI/Helpers/SMSSender.cs
@@ -0,0 +1,29 @@
+namespace AdministrationAPI.Helpers;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+public class SMSSender
+    {
+        private readonly IConfiguration _config;
+        public SMSSender(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void SendSMS(string phoneNumber, string activationCode)
+        {
+            // IN PROD, SAVE THESE SECRETS IN SOME SECRET MANAGER OR .env FILE
+            string accountSid = "AC3d1624658e0b068286de8622eecff9b3";
+            string authToken = _config["SI-Projekat:TwilioAuthToken"];
+            // string authToken = "955da57aa49192f0f431bdf0f12a151c";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: activationCode,
+                from: "SI-projekat",
+                to: new Twilio.Types.PhoneNumber("+387" + phoneNumber.Remove(0, 1))
+            );
+            //06xxxxxxx -> +3876xxxxxxx
+        }
+    }
