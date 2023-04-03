d1e7cd766ee5f6883e310d2be025649efd0e3286
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-04-04 01:24:48 +0200

    fe fixed

--- administration-app/src/App.js
+++ administration-app/src/App.js
@@ -5,12 +5,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VendorManagementModal from './components/vendor/VendorManagementModal';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
-
+import { NavBar } from './components/NavBar/NavBar';
+import { TransactionsList } from './components/TransactionsView/transactions/TransactionsList';
+import { Payment } from './components/Payment/Payment';
 function App() {
 	return (
 		<div className='App'>
 			<Router>
+				<NavBar />
 				<Routes>
+					<Route path='/' element={<h1>SI projekat</h1>} />
+					<Route path='/transactions' element={<TransactionsList />} />
+					<Route
+						path='/payment/:currency/:type/:recipientName/:transactionAmount/:recipientAccountNumber'
+						element={<Payment />}
+					/>
+					<Route path='/payment' element={<Payment />} />
 					<Route
 						path='/user'
 						element={
