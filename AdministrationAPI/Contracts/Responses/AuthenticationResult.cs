4631b179bcf742f41654d5a4e5822f25696ea757
Elvir Vlahvoljak <evlahovlja1@etf.unsa.ba>
2023-04-05 16:31:08 +0200

    return all user data, nullable

--- AdministrationAPI/Contracts/Responses/AuthenticationResult.cs
+++ AdministrationAPI/Contracts/Responses/AuthenticationResult.cs
@@ -8,5 +8,10 @@ namespace AdministrationAPI.Contracts.Responses
         public bool TwoFactorEnabled { get; set; }
         public string? UserId { get; set; }
         public IEnumerable<string> Errors { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
