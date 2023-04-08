28869550503b1c2e3379fa2ae3aec37c2c8bb1ff
Kenan Fejzić <92314295+kfejzic1@users.noreply.github.com>
2023-04-08 12:34:06 +0200

    deleted userid from localstorage (#13)

--- administration-app/src/components/User/UserProfile.js
+++ administration-app/src/components/User/UserProfile.js
@@ -31,7 +31,7 @@ const ProfilePage = () => {
 
 	useEffect(() => {
 		setIsLoading(true);
-		getUser(localStorage.getItem('userId')).then(res => {
+		getUser().then(res => {
 			setUser(res.data);
 			setIsLoading(false);
 			setIs2FAEnabled(res.data.isTwoFactorEnabled);
@@ -40,7 +40,7 @@ const ProfilePage = () => {
 	}, []);
 
 	const handle2FASetup = () => {
-		getTwoFactorQRCode(localStorage.getItem('userId')).then(res => {
+		getTwoFactorQRCode().then(res => {
 			setQrCode(res.data);
 		});
 
@@ -49,7 +49,7 @@ const ProfilePage = () => {
 
 	const toggle2FA = () => {
 		setIsLoading(true);
-		toggle2Factor(localStorage.getItem('userId')).then(res => {
+		toggle2Factor().then(res => {
 			if (res.data) setIs2FASettedUp(false);
 			setIs2FAEnabled(res.data);
 			setIsLoading(false);
