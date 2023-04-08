0eeb6967d5371eabe2720e8aa7b0516c1eed4cca
Jasmin Višća <ivisca1@etf.unsa.ba>
2023-04-08 22:34:04 +0200

    copied files from previous branch

---
+++ administration-app/src/services/userManagementService.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import { env } from '../config/env';
+
+export function createCustomer(request) {
+	return axios(env.API_ENV.url + '/api/Customer/createCustomer', {
+		method: 'POST',
+		data: request,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('token'),
+		},
+	});
+}
+
+export function setCustomerPassword(request) {
+	return axios(env.API_ENV.url + '/api/Customer/setCustomerPassword', {
+		method: 'POST',
+		data: request,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+}
+
+export function resetCustomerPassword(request) {
+	return axios(env.API_ENV.url + '/api/Customer/resetCustomerPassword', {
+		method: 'PATCH',
+		data: request,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+}
+
+export function getAllCustomers() {
+	return axios(env.API_ENV.url + '/api/User', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer' + localStorage.getItem('token'),
+		},
+	});
+}
+
+export function findCustomerById(id) {
+	return axios(env.API_ENV.url + `/api/Customer/getCustomer/${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer' + localStorage.getItem('token'),
+		},
+	});
+}
+
+export function editCustomer(editUserRequest) {
+	return axios(env.API_ENV.url + '/api/Customer/editCustomer', {
+		method: 'PATCH',
+		data: editUserRequest,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('token'),
+		},
+	});
+}
+
+export function requestPasswordReset(id) {
+	return axios(env.API_ENV.url + '/api/Customer/forgotCustomerPassword', {
+		method: 'POST',
+		data: id,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('token'),
+		},
+	});
+}
