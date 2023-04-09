c2520abc434670fdb3d363ff247643170d608fd6
velidimsirovic <92308400+velidimsirovic@users.noreply.github.com>
2023-04-10 01:06:44 +0200

    Transactions view back (#21)
    * change UserID type from int to string
    
    * add test route
    
    * add authorization
    
    * provide userId in GetTransactions
    
    * change usings
    
    * create TransactionCreateRequest
    
    * Add post route for creating Transactions
    
    * Remove using
    
    * check id of current user when accessing specific transaction

---
+++ AdministrationAPI/Contracts/Requests/Transactions/TransactionCreateRequest.cs
@@ -0,0 +1,13 @@
+using AdministrationAPI.Models.Transaction;
+
+namespace AdministrationAPI.Contracts.Requests;
+public class TransactionCreateRequest
+{
+    public TransactionType Type { get; set; }
+    public DateTime DateTime { get; set; }
+    public string Recipient { get; set; } = string.Empty;
+    public string Account { get; set; } = string.Empty;
+    public float Amount { get; set; }
+    public TransactionStatus Status { get; set; }
+    public string UserId { get; set; }
+}
