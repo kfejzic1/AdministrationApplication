1d4708adc17e0be8e70114b9b209b9a842281f2d
beganov1c <92276578+beganov1c@users.noreply.github.com>
2023-04-09 17:34:45 +0200

    One role per user

--- AdministrationAPI/Contracts/Responses/GetUserResponse.cs
+++ AdministrationAPI/Contracts/Responses/GetUserResponse.cs
@@ -6,7 +6,7 @@ namespace AdministrationAPI.Contracts.Responses
     public class GetUserResponse
     {
         public User user { get; set; }
-        public IEnumerable<IdentityRole> userRoles { get; set; }
+        public IdentityRole userRole { get; set; }
         public IEnumerable<IdentityRole> roles { get; set; }
     }
 }
