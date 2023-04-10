f62ca76d5e3e21b8d3abf0f9a7a97dcb6cb38a70
Filip Marić <fmaric1@etf.unsa.ba>
2023-04-10 11:31:14 +0200

    Template update fixed for amount and paymenttype

--- AdministrationAPI/Services/TemplateService.cs
+++ AdministrationAPI/Services/TemplateService.cs
@@ -7,14 +7,7 @@ namespace AdministrationAPI.Services
 {
     public class TemplateService : ITemplateService
     {
-        /*
-        private static List<Template> templates = new List<Template> {
-            new Template { Id=1, UserId="1", Title="Plin", Description="placanje racuna za plin", Currency="BAM", 
-            RecipientName="sarajevoGas", RecipientAccountNumber="123456" },
-            new Template { Id=2, UserId="2", Title="Voda", Description="placanje racuna za vodu", Currency="BAM", 
-            RecipientName="vodovod", RecipientAccountNumber="123457" }
-        };
-        */
+        
 
         private readonly TemplateDbContext _context;
 
@@ -53,6 +46,8 @@ namespace AdministrationAPI.Services
             }
             template.UserId=request.UserId;
             template.Title=request.Title;
+            template.Amount = request.Amount;
+            template.PaymentType = request.PaymentType;
             template.Description=request.Description;
             template.Currency=request.Currency;
             template.RecipientName=request.RecipientName;
