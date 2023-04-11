8590b5eed161298ff16758f70dddecb1a02a5938
AminaK <92340368+akurtovic4@users.noreply.github.com>
2023-04-12 01:51:54 +0200

    drag and drop imports

--- administration-app/src/components/vendor/dragAndDropModal/dragAndDropModal.js
+++ administration-app/src/components/vendor/dragAndDropModal/dragAndDropModal.js
@@ -9,84 +9,90 @@ import {
 } from '@mui/material';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { DropzoneArea } from 'material-ui-dropzone';
 
 const useStyles = makeStyles(theme => ({
 
   root: {
-	flexGrow: 1,
-	minHeight: '50%',
-	maxWidth: '90%',
-	margin: 'auto',
-	border: 'none',
-},
-card: {
-	border: 'none',
-	padding: '5px',
-},
-button: {
-	marginRight: '5%',
-	'&.MuiButton-contained': {
-		backgroundImage: 'linear-gradient(144deg, #ffb649 35%,#ffee00)',
-		borderRadius: '15px',
-		color: 'black',
-		width: '8rem',
-		'&:hover': {
-			backgroundImage: 'linear-gradient(144deg, #e9a642 65%,#e9de00)',
-			boxShadow: 'none',
-		},
-		'&:disabled': {
-			backgroundColor: '#ffffff',
-			boxShadow: 'none',
-			color: '#d3d3d3',
-		},
-	},
-},
-formControl: {
-	margin: theme.spacing(1),
-	minWidth: '95%',
-},
-paper: {
+    flexGrow: 1,
+    minHeight: '50%',
+    maxWidth: '90%',
+    margin: 'auto',
+    border: 'none',
+  },
+  card: {
+    border: 'none',
+    padding: '5px',
+  },
+  button: {
+    marginRight: '5%',
+    '&.MuiButton-contained': {
+      backgroundImage: 'linear-gradient(144deg, #ffb649 35%,#ffee00)',
+      borderRadius: '15px',
+      color: 'black',
+      width: '8rem',
+      '&:hover': {
+        backgroundImage: 'linear-gradient(144deg, #e9a642 65%,#e9de00)',
+        boxShadow: 'none',
+      },
+      '&:disabled': {
+        backgroundColor: '#ffffff',
+        boxShadow: 'none',
+        color: '#d3d3d3',
+      },
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: '95%',
+  },
+  paper: {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    backgroundColor: '#000',
+    backgroundColor: 'white',
     boxShadow: 24,
     padding: '1.5rem',
     borderRadius: '4px',
     outline: 'none',
+    zIndex: '9999999999', 
+    display: 'block',
+    width: '400px',
+    height: '300px',
   },
-chips: {
-	display: 'flex',
-	flexWrap: 'wrap',
-},
-chip: {
-	margin: 2,
-},
-textField: {
-	minWidth: '95%',
-},
-cardActions: {
-	justifyContent: 'right',
-	paddingTop: 20,
-},
+  
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
+  },
+  textField: {
+    minWidth: '95%',
+  },
+  cardActions: {
+    justifyContent: 'right',
+    paddingTop: 20,
+  },
 }));
 
 export default function UploadModal() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true); // change the initial state to true to make the modal visible
   const classes = useStyles();
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  const [files, setFiles] = useState([]);
 
   const handleClose = () => {
     setOpen(false);
   };
+  const handleFileChange = (newFiles) => {
+    setFiles([...files, ...newFiles]);
+  };
 
   return (
     <Box>
-      <Button onClick={handleOpen}>Upload</Button>
+      {/* Remove the button that opens the modal */}
       <Modal
         open={open}
         onClose={handleClose}
@@ -108,15 +114,28 @@ export default function UploadModal() {
             }}
           >
           
+         
           </IconButton>
+          <DropzoneArea onChange={handleFileChange} />
+          <Box sx={{ textAlign: 'center', pt: '20px' }}>
+            <Button onClick={handleClose}>Close</Button>
+          </Box>
           <Typography variant='body2' gutterBottom>
             Drop files here or click to select files to upload.
           </Typography>
           <Box sx={{ textAlign: 'center', pt: '20px' }}>
             <Button onClick={handleClose}>Close</Button>
           </Box>
+          <Box sx={{ textAlign: 'center', pt: '20px' }}>
+            <Button onClick={handleClose}>Close</Button>
+            <Typography variant='body2' gutterBottom>
+              tralalalalalal
+            </Typography>
+</Box>
         </Box>
       </Modal>
     </Box>
   );
 }
+
+
