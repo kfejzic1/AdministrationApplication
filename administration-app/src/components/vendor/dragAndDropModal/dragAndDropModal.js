76c41d85ae092ceeda869334beaf6ae9d4fed3db
Din Švraka <dsvraka1@etf.unsa.ba>
2023-04-13 15:30:04 +0200

    Dates

--- administration-app/src/components/vendor/dragAndDropModal/dragAndDropModal.js
+++ administration-app/src/components/vendor/dragAndDropModal/dragAndDropModal.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Modal, Typography, IconButton, Box, CloseIcon } from '@mui/material';
 
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { makeStyles } from '@material-ui/core/styles';
 import { DropzoneArea } from 'material-ui-dropzone';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
 
 const useStyles = makeStyles(theme => ({
 	paper: {
@@ -25,12 +29,15 @@ export default function UploadModal() {
 	const classes = useStyles();
 	const [files, setFiles] = useState([]);
 
+	const [dateStartFilter, setDateStartFilter] = useState(null);
+	const [dateEndFilter, setDateEndFilter] = useState(null);
 	const handleClose = () => {
 		setOpen(false);
 	};
 	const handleFileChange = newFiles => {
 		setFiles([...files, ...newFiles]);
 	};
+	const [value, setValue] = useState(dayjs('2022-04-17'));
 
 	return (
 		<Box className={classes.paper}>
@@ -47,6 +54,28 @@ export default function UploadModal() {
 					color: theme => theme.palette.grey[500],
 				}}></IconButton>
 			<DropzoneArea onChange={handleFileChange} />
+			
+			<Box sx={{display:'flex',flexDirection:'row'}}>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<DesktopDatePicker
+								label='Start'
+								sx={{ width: 'auto' }}
+								maxDate={dateEndFilter}
+								value={dateStartFilter}
+								onChange={a => {
+									setDateStartFilter(a);
+								}}
+							></DesktopDatePicker>
+							<DesktopDatePicker
+								minDate={dateStartFilter}
+								value={dateEndFilter}
+								onChange={a => {
+									setDateEndFilter(a);
+								}}
+								label='End'
+							></DesktopDatePicker>
+				</LocalizationProvider>
+			</Box>
 			<Box sx={{ textAlign: 'center', pt: '20px' }}>
 				<Button onClick={handleClose}>Close</Button>
 			</Box>
