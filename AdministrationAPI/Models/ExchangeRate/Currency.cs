208867c1f7ae3f6a597c2f5063b71a3295d8b65a
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-04-14 15:18:16 +0200

    Model and DataContext for currencies

---
+++ AdministrationAPI/Models/ExchangeRate/Currency.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Currency
+{
+	public string Id { get; set; }
+	public string Name { get; set; }
+	public bool Default { get; set; } = false;
+	public ICollection<ExchangeRate> ExchangeRates { get; set; }
+}
---
+++ AdministrationAPI/Models/ExchangeRate/ExchangeRate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class ExchangeRate
+{
+	public string Id { get; set; }
+	public string InputCurrencyId { get; set; }
+	public string OutputCurrencyId { get; set; }
+	public Currency InputCurrency { get; set; }
+	public Currency OutputCurrency { get; set; }
+	public DateOnly StartDate { get; set; }
+	public DateOnly? EndDate { get; set; }
+}
