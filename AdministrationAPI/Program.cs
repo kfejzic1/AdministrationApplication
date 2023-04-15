257fa862c4710b3e384a21f3e066f2e4beb96054
Elvir Vlahvoljak <evlahovlja1@etf.unsa.ba>
2023-04-15 22:44:41 +0200

    addedd exchangerate service to DI

--- AdministrationAPI/Program.cs
+++ AdministrationAPI/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IActivationCodeService, ActivationCodeService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<ITemplateService, TemplateService>();
+builder.Services.AddScoped<IExchangeRateService, ExchangeRateService>();
 
 
 builder.Services.AddControllers();
