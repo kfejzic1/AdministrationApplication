0499b7278de15c9474a66c6f0af01ae23d6d6284
Almina Brulić <abrulic1@etf.unsa.ba>
2023-05-02 13:31:17 +0200

    Added default vouchers to database

--- AdministrationAPI/Data/AppDbContext.cs
+++ AdministrationAPI/Data/AppDbContext.cs
@@ -200,7 +200,6 @@ namespace AdministrationAPI.Data
                    };
 
             builder.Entity<VoucherStatus>().HasData(voucherStatuses);
-                 
 
 
             // Seed Users
@@ -280,6 +279,18 @@ namespace AdministrationAPI.Data
                 new Account(){Id = -3, UserId = users[1].Id, CurrencyId = currencies[0].Id, AccountNumber = "3", Description = "Acc3", Approved = true, RequestDocumentPath = "/"}
             };
             builder.Entity<Account>().HasData(accounts);
+
+
+            //Seed Vouchers
+            List<Voucher> vouchers = new List<Voucher>()
+            {
+                new Voucher() { Id = 1, Amount = 50, CurrencyId = "1", Code = "12fg-4g2z-4gs2-gs35", VoucherStatusId = "0", CreatedBy = users[7].Id},
+                new Voucher() { Id = 2, Amount = 20, CurrencyId = "1", Code = "FDg4-DG4A-HS5A-HA36", VoucherStatusId = "0", CreatedBy = users[7].Id},
+                new Voucher() { Id = 3, Amount = 50, CurrencyId = "1", Code = "LLL4-GTA3-g4st-35h5", VoucherStatusId = "1", CreatedBy = users[7].Id},
+                new Voucher() { Id = 4, Amount = 50, CurrencyId = "1", Code = "kg45-fkai-3k5f-ek1f", VoucherStatusId = "2", CreatedBy = users[7].Id, RedeemedBy = users[6].Id} 
+            };
+
+            builder.Entity<Voucher>().HasData(vouchers);
 
         }
 
