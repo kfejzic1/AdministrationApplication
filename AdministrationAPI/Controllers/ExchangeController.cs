c4ba1513f7ea47bf36b23ce5d1c57fb8794dfab8
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-05-05 19:10:50 +0200

    auth reenabled

--- AdministrationAPI/Controllers/ExchangeController.cs
+++ AdministrationAPI/Controllers/ExchangeController.cs
@@ -13,7 +13,7 @@ namespace AdministrationAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-  // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+   [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ExchangeController : ControllerBase
     {
 
