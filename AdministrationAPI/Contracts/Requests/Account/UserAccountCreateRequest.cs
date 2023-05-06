11690aefb3823bd46dfd38a13733023609a883ab
beganov1c <92276578+beganov1c@users.noreply.github.com>
2023-05-06 14:24:33 +0200

    Removed approved prop from conrtact request

--- AdministrationAPI/Contracts/Requests/Account/UserAccountCreateRequest.cs
+++ AdministrationAPI/Contracts/Requests/Account/UserAccountCreateRequest.cs
@@ -7,7 +7,6 @@ namespace AdministrationAPI.Contracts.Requests.Users
         public string CurrencyId { get; set; }
         public string Description { get; set; }
         public string? RequestDocumentPath { get; set; }
-        public bool Approved { get; set; }
         public string? UserId { get; set; }
     }
 }
