53109c28259638753e45565888c4b32a911ed2b7
beganov1c <92276578+beganov1c@users.noreply.github.com>
2023-05-08 12:22:35 +0200

    Admin id deleted from ClaimAcceptRequest

--- AdministrationAPI/Contracts/Requests/Transactions/ClaimAcceptRequest.cs
+++ AdministrationAPI/Contracts/Requests/Transactions/ClaimAcceptRequest.cs
@@ -5,11 +5,6 @@ namespace AdministrationAPI.Contracts.Requests.Transactions
     public class ClaimAcceptRequest
     {
         public int TransactionClaimId { get; set; }
-        public string AdminId { get; set; }
 
-        public ClaimAcceptRequest()
-        {
-            AdminId = string.Empty;
-        }
     }
 }
