02c597998e3a6a186fd5cfdd86633b77ef0eb95d
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-05-10 12:50:26 +0200

    added new rute for exchange

---
+++ AdministrationAPI/Contracts/Requests/Exchange/ExchangeTransactionRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AdministrationAPI.Contracts.Requests.Exchange
+{
+    public class ExchangeTransactionRequest
+    {
+        public double Amount { get; set; }
+        public string TransactionPurpose { get; set; }
+        public string SenderCurrency { get; set; }
+        public string RecipientCurrnecy { get; set; }
+    }
+}
