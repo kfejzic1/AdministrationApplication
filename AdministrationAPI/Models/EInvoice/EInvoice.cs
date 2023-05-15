5294897e386411483143727ba81cdeb904a0e0de
Elvir Vlahovljak <73286824+evlahovlja1@users.noreply.github.com>
2023-05-15 12:41:53 +0200

    Einvoice registration request (#118)
    * route for e-invoice requests
    
    * invoices by vendor id
    
    * e-invoice request model changes
    
    * Small fixes
    
    * Route for request handling
    
    * Minor fixes - route for request handling
    
    * defining required data for vendors route
    
    * created invoice request and retrieval of b2b req data
    
    * data seeded double fixed
    
    * EInvoice model created
    
    * Model fix
    
    ---------
    
    Co-authored-by: Nikolina Kokor <nkokor@icloud.com>
    Co-authored-by: isthisfahret <73430835+isthisfahret@users.noreply.github.com>
    Co-authored-by: beganov1c <92276578+beganov1c@users.noreply.github.com>

---
+++ AdministrationAPI/Models/EInvoice/EInvoice.cs
@@ -0,0 +1,25 @@
+﻿using Mysqlx.Crud;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdministrationAPI.Models.EInvoice
+{
+    public class EInvoice
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public int Id { get; set; }
+        public string PayerName { get; set; }
+        public int PayerAccountNumber { get; set; } 
+        public string PayerAdress { get; set; }
+        public string Reference { get; set; }
+        public string PayeeName { get; set; }
+        public int PayeeAccountNumber { get; set; }
+        public string PayeeAdress { get; set; }
+        public int Amount { get; set; }
+        public int CurrencyId { get; set; }
+        public Currency Currency { get; set; }
+
+    }
+}
