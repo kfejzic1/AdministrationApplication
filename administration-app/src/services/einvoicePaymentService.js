eb36da927a9c4cd9b8c3afdd9d4c513064b9cac5
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2023-05-16 00:43:07 +0200

    Removed mock data

--- administration-app/src/services/einvoicePaymentService.js
+++ administration-app/src/services/einvoicePaymentService.js
@@ -2,39 +2,27 @@ import axios from 'axios';
 import { env } from '../config/env';
 
 export function getAllInvoices() {
-    return [{id: 1, 
-    PayerName: 'Matija Kokor', 
-    PayerAccountNumber: '1234', 
-    PayerAddress: 'ADRESA', 
-    Reference: 'REFERENCE', 
-    PayeeName: 'Test Company', 
-    PayeeAccountNumber: '8hae7i1', 
-    PayeeAddress: 'ADDRESS', 
-    Description: 'Bill Payment',
-    Amount: 37, 
-    CurrencyId: 7, 
-    Currency: {Name:'bam'}}];
-	// return axios(env.API_ENV.url + '/api/EInvoice/listEInvoices', {
-	// 	method: 'GET',
-	// 	headers: {
-	// 		Authorization: 'Bearer ' + localStorage.getItem('token'),
-	// 		'Content-Type': 'application/json',
-	// 	},
-	// });
+    return axios(env.API_ENV.url + '/api/EInvoice/listEInvoices', {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+            'Content-Type': 'application/json',
+        },
+    });
 }
 
-export function setPaidInvoice(id){
+export function setPaidInvoice(id) {
     return axios(env.API_ENV.url + '/api/EInvoice/payEinvoice/' + id, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: 'Bearer ' + localStorage.getItem('token'),
-		},
-	});
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+    });
 }
 
-export function createEInvoiceTransaction(request){
-    return axios('https://processingserver.herokuapp.com/api/EInvoicePayment/ExecuteInvoicePayment?token='+localStorage.getItem('token'),{
+export function createEInvoiceTransaction(request) {
+    return axios('https://processingserver.herokuapp.com/api/EInvoicePayment/ExecuteInvoicePayment?token=' + localStorage.getItem('token'), {
         method: 'POST',
         data: request,
         headers: {
